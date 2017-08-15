e492b3eeedbd7c06c68fab342dbbb6829572bffb
sunlongtao <492037876@qq.com>
2017-08-15 12:01:21 +0800

    1

---
+++ AuthManager.java
@@ -0,0 +1,28 @@
+package com.o2o.business.common.bll;
+
+import com.o2o.common.bean.TblPartnerApi;
+import com.o2o.business.common.dao.TblPartnerApiDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * (C) 2014-2016, O2OMedical Inc. All rights reserved.
+ *
+ * @author musilin
+ * @date 2016年03月17日
+ */
+@Component
+public class AuthManager {
+    @Autowired
+    private TblPartnerApiDao tblPartnerApiDao;
+
+
+    public TblPartnerApi findPartnerApiInformationByName(String partnerName){
+        Map<String, Object> filter = new HashMap<String, Object>(1);
+        filter.put("partnerName", partnerName);
+        return this.tblPartnerApiDao.findUnique("findPartnerApiInformationByName", filter);
+    }
+}
